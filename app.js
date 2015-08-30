2bbe368cb9b4429d3c79f4c3040a9e2665f413c5
unknown <pisces_naila@yahoo.com>
2015-08-30 02:24:36 -0700

    new commit

---
+++ app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by a on 8/27/15.
+ */
+var chatRooms = [{ id: 1, topic: "Sports" }, { id: 2, topic: "Fun" }, { id: 3, topic: "Social" }];
+var user = [{ id: 1, name: "user01", room: 1 }, { id: 2, name: "user02", room: 1 }, { id: 3, name: "user03", room: 2 }, { id: 4, name: "user04", room: 2 }, { id: 5, name: "user05", room: 3 }, { id: 5, name: "user05", room: 3 }];
+//# sourceMappingURL=app.js.map
